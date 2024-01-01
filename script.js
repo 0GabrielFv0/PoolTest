fe6c3acd452739d77eb7a8eb9e41493763ef611b
Gabriel <110939144+0GabrielFv0@users.noreply.github.com>
2024-01-01 18:03:40 -0300

    s

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
 function calcularRecomendacoes() {
-    // Obter valores dos campos de entrada
     var ph = parseFloat(document.getElementById('ph').value);
     var cloro = parseFloat(document.getElementById('cloro').value);
     var alcalinidade = parseFloat(document.getElementById('alcalinidade').value);
     var litragem = parseFloat(document.getElementById('litragem').value);
 
-    // Realizar cálculos ou ações desejadas com esses valores
-    // Por exemplo, você pode exibir alertas com recomendações com base nos valores inseridos
-    alert("Valores inseridos: pH=" + ph + ", Cloro=" + cloro + ", Alcalinidade=" + alcalinidade + ", Litragem=" + litragem);
+    var recomendacoes = obterRecomendacoes(ph, cloro, alcalinidade, litragem);
+
+    exibirRecomendacoes(recomendacoes);
+}
+
+function obterRecomendacoes(ph, cloro, alcalinidade, litragem) {
+    var recomendacoes = {};
+
+    if (ph < 7.2) {
+        recomendacoes.produto1 = "Alcalinizador de pH";
+    } else if (ph > 7.8) {
+        recomendacoes.produto1 = "Redutor de pH";
+    }
+
+    if (cloro < 1.0) {
+        recomendacoes.produto2 = "Cloro líquido";
+    } else if (cloro > 3.0) {
+        recomendacoes.produto2 = "Redutor de cloro";
+    }
+
+    if (alcalinidade < 80) {
+        recomendacoes.produto3 = "Elevador de Alcalinidade";
+    } else if (alcalinidade > 120) {
+        recomendacoes.produto3 = "Redutor de Alcalinidade";
+    }
+
+    if (litragem < 5000) {
+        recomendacoes.produto4 = "Suplemento de Litragem";
+    } else if (litragem > 10000) {
+        recomendacoes.produto4 = "Estabilizador de Litragem";
+    }
+
+    return recomendacoes;
+}
+
+function exibirRecomendacoes(recomendacoes) {
+    var resultadosDiv = document.getElementById('recomendacoes');
+    resultadosDiv.innerHTML = "<h2>Recomendações:</h2>";
+
+    if (Object.keys(recomendacoes).length === 0) {
+        resultadosDiv.innerHTML += "<p>Nenhuma recomendação no momento.</p>";
+    } else {
+        for (var produto in recomendacoes) {
+            if (recomendacoes.hasOwnProperty(produto)) {
+                resultadosDiv.innerHTML += "<p><strong>" + produto + ":</strong> " + recomendacoes[produto] + "</p>";
+            }
+        }
+    }
 }

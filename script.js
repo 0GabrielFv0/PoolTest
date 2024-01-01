0cf029c77061a2e5acdbe626bf77cdbe3fb1b67e
Gabriel <110939144+0GabrielFv0@users.noreply.github.com>
2024-01-01 17:59:50 -0300

    s

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function irParaPaginaPrincipal() {
-    // Redirecionar para a página principal
-    window.location.href = "pagprinc.html"; // Substitua com o nome do seu arquivo principal
+function calcularRecomendacoes() {
+    // Obter valores dos campos de entrada
+    var ph = parseFloat(document.getElementById('ph').value);
+    var cloro = parseFloat(document.getElementById('cloro').value);
+    var alcalinidade = parseFloat(document.getElementById('alcalinidade').value);
+    var litragem = parseFloat(document.getElementById('litragem').value);
+
+    // Realizar cálculos ou ações desejadas com esses valores
+    // Por exemplo, você pode exibir alertas com recomendações com base nos valores inseridos
+    alert("Valores inseridos: pH=" + ph + ", Cloro=" + cloro + ", Alcalinidade=" + alcalinidade + ", Litragem=" + litragem);
 }

c67f0562af40a90e19a8deeef2ba74faabf5011e
Gabriel <110939144+0GabrielFv0@users.noreply.github.com>
2024-01-02 21:34:46 -0300

    s

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
+// script.js
+
 function obterRecomendacoes(ph, cloro, alcalinidade, litragem) {
     var recomendacoes = {};
 
     if (ph < 7.2) {
         recomendacoes.produto1 = "Alcalinizador de pH";
-        recomendacoes.dosagem1 = calcularDosagem(recomendacoes.produto1, litragem);
     } else if (ph > 7.8) {
         recomendacoes.produto1 = "Redutor de pH";
-        recomendacoes.dosagem1 = calcularDosagem(recomendacoes.produto1, litragem);
     }
 
     if (cloro < 1.0) {
         recomendacoes.produto2 = "Cloro líquido";
-        recomendacoes.dosagem2 = calcularDosagem(recomendacoes.produto2, litragem);
     } else if (cloro > 3.0) {
         recomendacoes.produto2 = "Redutor de cloro";
-        recomendacoes.dosagem2 = calcularDosagem(recomendacoes.produto2, litragem);
     }
 
     if (alcalinidade < 8) {
         recomendacoes.produto3 = "Elevador de Alcalinidade";
-        recomendacoes.dosagem3 = calcularDosagem(recomendacoes.produto3, litragem);
     } else if (alcalinidade > 12) {
         recomendacoes.produto3 = "Redutor de Alcalinidade";
-        recomendacoes.dosagem3 = calcularDosagem(recomendacoes.produto3, litragem);
     }
-
 
     return recomendacoes;
 }
 
-function calcularRecomendacoes() {
-    var ph = parseFloat(document.getElementById('ph').value);
-    var cloro = parseFloat(document.getElementById('cloro').value);
-    var alcalinidade = parseFloat(document.getElementById('alcalinidade').value);
-    var litragem = parseFloat(document.getElementById('litragem').value);
-
-    var recomendacoes = obterRecomendacoes(ph, cloro, alcalinidade, litragem);
-    exibirRecomendacoes(recomendacoes);
-}
-
-function exibirRecomendacoes(recomendacoes) {
-    var recomend
-
+function calcularDosagem(produto, litragem) {
+    var dosagemPorLitro = 0;
 
     switch (produto) {
         case "Alcalinizador de pH":
@@ -69,4 +53,30 @@ function exibirRecomendacoes(recomendacoes) {
     }
 
     return (litragem * dosagemPorLitro).toFixed(2);
+}
+
+function calcularRecomendacoes() {
+    var ph = parseFloat(document.getElementById('ph').value);
+    var cloro = parseFloat(document.getElementById('cloro').value);
+    var alcalinidade = parseFloat(document.getElementById('alcalinidade').value);
+    var litragem = parseFloat(document.getElementById('litragem').value);
+
+    var recomendacoes = obterRecomendacoes(ph, cloro, alcalinidade, litragem);
+    exibirRecomendacoes(recomendacoes, litragem);
+}
+
+function exibirRecomendacoes(recomendacoes, litragem) {
+    var recomendacoesElement = document.getElementById('recomendacoes');
+    recomendacoesElement.innerHTML = '';
+
+    for (var produto in recomendacoes) {
+        if (recomendacoes.hasOwnProperty(produto)) {
+            var dosagemPorLitro = calcularDosagem(recomendacoes[produto], litragem);
+            var textoRecomendacao = produto + ': ' + recomendacoes[produto] + ' - Dosagem: ' + dosagemPorLitro + ' L';
+
+            var pElement = document.createElement('p');
+            pElement.textContent = textoRecomendacao;
+            recomendacoesElement.appendChild(pElement);
+        }
+    }
 }
